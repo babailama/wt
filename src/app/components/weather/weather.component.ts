0af1b90105cc5d1ccca0325611410d79eb6fb269
andivanov@gmail.com <@DNAPC.nafta.priv>
2017-04-24 22:57:55 +0300

    table interface fuckup

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 import { WeatherDataService } from '../../services/weather-data/weather-data.service';
+import { WeatherData2TableService } from '../../services/weather-data2-table/weather-data2-table.service';
 import { WeatherDataInteface } from '../../interfaces/weather-data.interface';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css'],
-  providers: [WeatherDataService]
+  providers: [WeatherDataService, WeatherData2TableService]
 })
 export class WeatherComponent implements OnInit {
   errorMessage: string;
   data: WeatherDataInteface[];
   private weatherData: WeatherDataInteface[];
-  constructor(private weatherDataService: WeatherDataService) { }
+  constructor(private weatherDataService: WeatherDataService,
+              private weatherData2TableService: WeatherData2TableService) { }
 
   getWeatherData(): void {
     this.weatherDataService.getOData().subscribe(
@@ -23,6 +25,7 @@ export class WeatherComponent implements OnInit {
 
   ngOnInit() {
     this.getWeatherData();
+    const a = this.weatherData2TableService.getWeatherTable();
   }
 
 }
